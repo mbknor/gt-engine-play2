3492d67d160bfa71e2dfb460b01946647da9ddab
Morten Kjetland <mbk@kjetland.com>
2012-01-19 23:04:43 +0100

    added Play dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object GTEnginePlayBuild extends Build {
 
   val mbknorGithubRepoUrl = "http://mbknor.github.com/m2repo/releases/"
-
+  val typesafeRepoUrl = "http://repo.typesafe.com/typesafe/releases/"
 
   lazy val GTEnginePlayProject = Project(
     "gt-engine-play",
@@ -15,7 +15,7 @@ object GTEnginePlayBuild extends Build {
                 publishTo := Some(Resolvers.mbknorRepository),
                 scalacOptions ++= Seq("-Xlint","-deprecation", "-unchecked","-encoding", "utf8"),
                 javacOptions ++= Seq("-encoding", "utf8", "-g"),
-                resolvers ++= Seq(DefaultMavenRepository, Resolvers.mbknorGithubRepo)
+                resolvers ++= Seq(DefaultMavenRepository, Resolvers.mbknorGithubRepo, Resolvers.localPlayRepo, Resolvers.typesafe)
             )
         )
 
@@ -23,12 +23,15 @@ object GTEnginePlayBuild extends Build {
   object Resolvers {
       val mbknorRepository = Resolver.ssh("my local mbknor repo", "localhost", "~/projects/mbknor.github.com/m2repo/releases/")(Resolver.mavenStylePatterns)
       val mbknorGithubRepo = "mbknor github Repository" at mbknorGithubRepoUrl
+      val localPlayRepo = Resolver.file("Play Local Repository", file("/Users/mortenkjetland/tmp/mbkplay/Play20/repository/local/"))(Resolver.ivyStylePatterns)
+      val typesafe = "Typesafe Repository" at typesafeRepoUrl
   }
 
   object Dependencies {
 
       val runtime = Seq(
-        "kjetland" % "gt-engine_2.9.1" % "0.1.7"
+        "kjetland" % "gt-engine_2.9.1" % "0.1.7",
+        "play" %% "play" % "2.0-RC1-SNAPSHOT"
       )
   }
 
